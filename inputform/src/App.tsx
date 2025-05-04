d797821510edd868c161ae37bbd44a72f8bb66eb
Erik Pohl <iotasubscript@gmail.com>
2025-05-03 21:20:53 -0400

    cleaned up code

--- inputform/src/App.tsx
+++ inputform/src/App.tsx
@@ -1,12 +1,7 @@
-
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback } from "react";
 import "./App.css";
 import TitanicData from "./titanic.json";
 import Counter from "./Counter";
-import { data } from "./Constants";
-import DataTable from "./DataTable";
-import MyButton from "./MyButton";
-import GenericTable from "./GenericTable";
 
 interface ToggleProps {
   initialValue: number;
@@ -24,17 +19,17 @@ const Toggle: React.FC<ToggleProps> = ({ initialValue, onToggle }) => {
 
   return (
     <button onClick={toggle} name="survival button">
-      {isOn ? "Survived" : "Did not survive"} 
+      {isOn ? "Survived" : "Did not survive"}
     </button>
   );
 };
 function App() {
   let myname: string = "Erik";
 
-  let filteredItems = TitanicData.filter(item => item.Survived === "0");
+  let filteredItems = TitanicData.filter((item) => item.Survived === "0");
 
   const [toggleState, setToggleState] = useState<number>(0);
-  
+
   const handleToggleChange = (newValue: number) => {
     setToggleState(newValue);
   };
@@ -43,11 +38,20 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Toggle whether survived or did not </h1>
-        <Toggle initialValue={toggleState} onToggle={handleToggleChange}/>
-        <h1>Filtered count {TitanicData.filter(item => Number(item.Survived) === toggleState).length}</h1>
+        <Toggle initialValue={toggleState} onToggle={handleToggleChange} />
+        <h1>
+          Filtered count{" "}
+          {
+            TitanicData.filter((item) => Number(item.Survived) === toggleState)
+              .length
+          }
+        </h1>
         <h2>TitanicCounter</h2>
-        <Counter data={TitanicData.filter(item => Number(item.Survived) === toggleState)}/>
-          
+        <Counter
+          data={TitanicData.filter(
+            (item) => Number(item.Survived) === toggleState
+          )}
+        />
       </header>
     </div>
   );
