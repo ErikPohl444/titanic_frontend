951ba5a7f323c0c702c02721d39d405c9aae9c61
Erik Pohl <iotasubscript@gmail.com>
2025-05-04 10:35:31 -0400

    isolated toggle buttons

--- inputform/src/App.tsx
+++ inputform/src/App.tsx
@@ -1,57 +1,41 @@
-import React, { useState, useCallback } from "react";
+import React, { useState} from "react";
 import "./App.css";
 import TitanicData from "./titanic.json";
 import Counter from "./Counter";
+import GenderToggle from "./GenderToggle";
+import SurvivedToggle from "./SurvivedToggle";
 
-interface ToggleProps {
-  initialValue: number;
-  onToggle: (value: number) => void;
-}
-
-const Toggle: React.FC<ToggleProps> = ({ initialValue, onToggle }) => {
-  const [isOn, setIsOn] = useState<number>(initialValue);
-
-  const toggle = useCallback(() => {
-    const newValue = isOn === 0 ? 1 : 0;
-    setIsOn(newValue);
-    onToggle(newValue);
-  }, [isOn, onToggle]);
-
-  return (
-    <button onClick={toggle} name="survival button">
-      {isOn ? "Survived" : "Did not survive"}
-    </button>
-  );
-};
 function App() {
-  let myname: string = "Erik";
-
-  let filteredItems = TitanicData.filter((item) => item.Survived === "0");
-
   const [toggleState, setToggleState] = useState<number>(0);
+  const [genderToggleState, setGenderToggleState] = useState<string>("male");
 
   const handleToggleChange = (newValue: number) => {
     setToggleState(newValue);
   };
 
+  const handleGenderToggleChange = (newValue: string) => {
+    setGenderToggleState(newValue);
+  };
+
+  const survivedGenderFilteredData = TitanicData.filter(
+    (item) =>
+      Number(item.Survived) === toggleState && item.Sex === genderToggleState
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Toggle whether survived or did not </h1>
-        <Toggle initialValue={toggleState} onToggle={handleToggleChange} />
-        <h1>
-          Filtered count{" "}
-          {
-            TitanicData.filter((item) => Number(item.Survived) === toggleState)
-              .length
-          }
-        </h1>
-        <h2>TitanicCounter</h2>
-        <Counter
-          data={TitanicData.filter(
-            (item) => Number(item.Survived) === toggleState
-          )}
+        <SurvivedToggle initialValue={toggleState} onToggle={handleToggleChange} />
+        <h1>Toggle gender</h1>
+        <GenderToggle
+          initialValue={genderToggleState}
+          onToggle={handleGenderToggleChange}
         />
+
+        <h1>Filtered count {survivedGenderFilteredData.length}</h1>
+        <h2>TitanicCounter</h2>
+        <Counter data={survivedGenderFilteredData} />
       </header>
     </div>
   );
