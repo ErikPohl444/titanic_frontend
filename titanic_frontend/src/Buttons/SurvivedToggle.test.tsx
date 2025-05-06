34e71b01461d07aa87aa946e466917f5c5259e09
Erik Pohl <erik.pohl.444@gmail.com>
2025-05-05 21:31:45 -0400

    fixed survived toggle test

--- titanic_frontend/src/Buttons/SurvivedToggle.test.tsx
+++ titanic_frontend/src/Buttons/SurvivedToggle.test.tsx
@@ -4,42 +4,42 @@ import SurvivedToggle from "./SurvivedToggle";
 
 describe("SurvivedToggle Component", () => {
   test("renders with the initial value", () => {
-    render(<SurvivedToggle initialValue=0 onToggle={jest.fn()} />);
-    const button = screen.getByRole("button", { name: "male" });
-    expect(button).toHaveTextContent("male");
+    render(<SurvivedToggle initialValue={0} onToggle={jest.fn()} />);
+    const button = screen.getByRole("button", { name: "Did not survive" });
+    expect(button).toHaveTextContent("Did not survive");
   });
 
-  test("toggles from male to female", () => {
+  test("toggles from Did not survive to Survived", () => {
     const mockOnToggle = jest.fn();
-    render(<GenderToggle initialValue="male" onToggle={mockOnToggle} />);
+    render(<SurvivedToggle initialValue={0} onToggle={mockOnToggle} />);
 
-    const button = screen.getByRole("button", { name: "male" });
+    const button = screen.getByRole("button", { name: "Did not survive" });
     fireEvent.click(button);
 
-    expect(button).toHaveTextContent("female");
-    expect(mockOnToggle).toHaveBeenCalledWith("female");
+    expect(button).toHaveTextContent("Survived");
+    expect(mockOnToggle).toHaveBeenCalledWith(1);
   });
 
-  test("toggles from female to male", () => {
+  test("toggles from Survived to Did not survive", () => {
     const mockOnToggle = jest.fn();
-    render(<GenderToggle initialValue="female" onToggle={mockOnToggle} />);
+    render(<SurvivedToggle initialValue={1} onToggle={mockOnToggle} />);
 
-    const button = screen.getByRole("button", { name: "female" });
+    const button = screen.getByRole("button", { name: "Survived" });
     fireEvent.click(button);
 
-    expect(button).toHaveTextContent("male");
-    expect(mockOnToggle).toHaveBeenCalledWith("male");
+    expect(button).toHaveTextContent("Did not survive");
+    expect(mockOnToggle).toHaveBeenCalledWith(0);
   });
 
   test("calls onToggle callback with the correct value", () => {
     const mockOnToggle = jest.fn();
-    render(<GenderToggle initialValue="male" onToggle={mockOnToggle} />);
+    render(<SurvivedToggle initialValue={0} onToggle={mockOnToggle} />);
 
-    const button = screen.getByRole("button", { name: "male" });
+    const button = screen.getByRole("button", { name: "Did not survive" });
     fireEvent.click(button);
-    expect(mockOnToggle).toHaveBeenCalledWith("female");
+    expect(mockOnToggle).toHaveBeenCalledWith(1);
 
     fireEvent.click(button);
-    expect(mockOnToggle).toHaveBeenCalledWith("male");
+    expect(mockOnToggle).toHaveBeenCalledWith(0);
   });
 });
